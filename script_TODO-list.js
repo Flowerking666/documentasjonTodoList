e5ca8004a98dda8e25687c1e0837713caa27ef17
Flowerking666 <116725455+Flowerking666@users.noreply.github.com>
2022-11-29 08:52:51 +0100

    Add files via upload

--- script_TODO-list.js
+++ script_TODO-list.js
@@ -1,6 +1,6 @@
 var conTasks = 0;
 var nr = 0; 
-
+// When pushed add button 
 function addElement(){
   var nodeList = document.getElementsByTagName("li");
   var li = document.createElement("li");
@@ -16,32 +16,33 @@ function addElement(){
 
   li.appendChild(check);
   li.appendChild(t);
-
+  //Error when input is empty and add button is pushed
   if (inputValue === '') {
     alert("Nothing to add")
   } else {
       document.getElementById('list').appendChild(li);
   }
-  document.getElementById("teller").innerHTML = conTasks+" / "+nr;
+  document.getElementById("counter").innerHTML = conTasks+" / "+nr;
 }
 
 function checked(obj){
   var checkBox = obj.getElementsByTagName("input");
-
+  //Changes the style to line through
   if (obj.getAttribute("style") != null && obj.getAttribute("style").toString() == "text-decoration:line-through"){
     obj.setAttribute("style", "");
   }
   else {
     obj.setAttribute("style", "text-decoration:line-through");
   }
+  //Checks if checkbox is checked and does math when it is and different math when it's not
   if (checkBox[0].checked == true){
     conTasks = conTasks + 1;
   } else {
     conTasks = conTasks - 1;
   }
-  document.getElementById("teller").innerHTML = conTasks+" / "+nr;
+  document.getElementById("counter").innerHTML = conTasks+" / "+nr;
 }
-//
+//Set the input field in focus
 window.onload = function() {
   document.getElementById("input1").focus();
 };
